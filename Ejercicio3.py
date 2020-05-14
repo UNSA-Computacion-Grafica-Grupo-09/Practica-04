7747dfe7d949a3d5fdd05b4f8078808cda633b72
Williamhyuk <williamhyuk@gmail.com>
2020-05-14 18:52:26 -0500

    ejercicio3

---
+++ Ejercicio3.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 13 09:46:02 2020
+@author: Williamhyuk
+"""
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+#obtenemos la imagen en escala de grises y tambbien loutilizamos para guardar el resultado de la imagen
+imagen_gray = cv2.imread('log_12.jpg', cv2.IMREAD_GRAYSCALE)
+imagen_resultado = cv2.imread('log_12.jpg', cv2.IMREAD_GRAYSCALE)
+
+alto,ancho = img.shape #Calculamos la dimencion de la imagen
+print(alto,ancho)
+
+cv2.imshow('Original',imagen_gray) #Convertimos a grises
+
+hist = cv2.calcHist([imagen_gray], [0], None, [256], [0, 256])
+
+
+#Inicializamos lo valores de c para hacer nuestras pruebas
+c=50
+
+
+#Utilizremos operador punto:
+
+for i in range(alto):
+    for j in range(ancho):
+        raiz=c*(np.sqrt(imagen_gray[i][j]))#Aplicamos nuestro operador raiz
+        #log=c*(np.log10(img[i][j]))
+        
+        if(raiz<0):# Si el resultado del pixel es un valor menor que 0 
+            imagen_resultado[i][j]=0#Se le asignara 0
+        elif(raiz>255):# Si el resultado del pixel es un valor menor que 255
+            imagen_resultado[i][j]=255#Se le asignara 0
+        else:
+            imagen_resultado[i][j]=raiz# Si los valores estan entre el rango de [0,255] se guardan sin mofiicacion
+
+
+#Calculamos histogramas para ver su funcionamiento
+
+hisR = cv2.calcHist([imagen_resultado], [0], None, [256], [0, 256])
+
+
+cv2.imshow('Imagen Resultado',imagen_resultado) #mostramos el resultado
+
+cv2.imwrite('res.jpg',imagen_resultado) #guardamos el resultado
+plt.plot(hist, color='blue' ) 
+plt.plot(hisR, color='red' )
+
+
+plt.xlabel('intensidad de iluminacion')
+plt.ylabel('cantidad de pixeles')
+plt.show()
+
+cv2.destroyAllWindows()
+cv2.destroyAllWindows()
