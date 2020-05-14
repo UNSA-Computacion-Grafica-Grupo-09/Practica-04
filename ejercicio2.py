4207e4bb5c65f7304880ea611be74b1c65da621b
Milagros Cruz <mcruzmam@unsa.edu.pe>
2020-05-13 19:37:51 -0500

    ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -13,27 +13,37 @@ imagen = cv2.imread('exp_5.jpg')
 
 imagen_resultado = cv2.imread('exp_5.jpg')
 
-imagen_original = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
+imagen_gray = cv2.imread('exp_5.jpg', cv2.IMREAD_GRAYSCALE)
+
 imagen_resultado = cv2.cvtColor(imagen_resultado, cv2.COLOR_BGR2RGB)
 
 
-array_imagen = np.asarray(imagen_original)
-print(array_imagen)
-
 #Implementacion de algoritmo de Operador logaritmico 
+
 #Inicializamos los valores
 
-c = 70
+#c = 40
+#c = 70
+#c = 90
+#c = 100
+c = 209
+#c = 210
 
-alto, ancho, canales = imagen_original.shape
+
+alto, ancho = imagen_gray.shape
 
 def operador_punto(pixel_RGB):
-    return (c * (np.log10(1 + pixel_RGB)))
+    pixel = 1 + pixel_RGB
+    loga = np.log10(pixel)
+    while(loga.all() <= 255 and loga.all() > 0):
+        return (c * (loga))
+    
+
 
 for x in range(alto):
     for y in range(ancho):
-        imagen_resultado[x][y] = operador_punto(imagen_original[x][y])
+        imagen_resultado[x][y] = operador_punto(imagen_gray[x][y])
         
 print(operador_punto(imagen_resultado))
 plt.imshow(imagen_resultado)
-plt.savefig('Imagen_resultado.jpg', bbox_inches='tight')
+plt.savefig('Imagen_resultado101.jpg', bbox_inches='tight')
